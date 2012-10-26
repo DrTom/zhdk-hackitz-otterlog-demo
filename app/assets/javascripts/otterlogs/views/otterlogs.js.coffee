4a8f1fb51530c8f556167507b66bcc8b1e5c13f6
Thomas Schank <DrTom@schank.ch>
2012-10-26 16:05:20 +0200

    Showing the otterlogs

--- app/assets/javascripts/otterlogs/views/otterlogs.js.coffee
+++ app/assets/javascripts/otterlogs/views/otterlogs.js.coffee
@@ -2,10 +2,24 @@ Otterlogs.Views.Otterlogs = Backbone.View.extend
 
   template: JST['otterlogs/templates/otterlogs']
 
+  row_template: JST['otterlogs/templates/otterlog_tr']
+
   initialize: ->
+    @collection = @options.collection
+
+    @collection.on "add", (model)=>
+      @add_row(model)
 
   render: ->
     @setElement($("#content"))
     @$el.empty()
     @$el.append @template()
+
+    @collection.models.forEach (model)=>
+      @add_row(model)
+  
+  add_row: (model)->
+    @$('table.otterlogs').append @row_template({model:model})
+
+
 
--- spec/factories/otterlogs.rb
+++ spec/factories/otterlogs.rb
@@ -1,8 +1,30 @@
+# -*- encoding : utf-8 -*-
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :otterlog do
     name { Faker::Name.first_name}
     id { SecureRandom.uuid }
+
+    documented_at {DateTime.now}
+
+    state do
+      states= ['fröhlich', 'neutral', 'traurig', 'schlafend', 'sich putzend', 'kopulierend']
+      states[rand(states.size)]
+    end
+    
+    place do
+      placeѕ= ['Am Ufer', 'auf dem grossen Stein', 'im Wasser', 'in Schlafbox A', 'in Schlafbox B']
+      placeѕ[rand(placeѕ.size)]
+    end
+
+    wetness do
+      wetnesses= ['nass', 'trocken', 'langsam trocknend']
+      wetnesses[rand(wetnesses.size)]
+    end
+
+    is_eating { rand(1) == 0}
+
   end
 end
