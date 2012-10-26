2a0598475b2f4aa47ebece004740a35eeb86dec8
Thomas Schank <DrTom@schank.ch>
2012-10-26 10:51:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackitzOtterlog::Application.config.secret_token = 'a88a192b8a56785ac6b19e98039e359667a17cadf7de89186e81541eacb76262e0a3f111a5341705a411253b3b0a0b0b3809569b5c9178aed63aa916f62d1980'
