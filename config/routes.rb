8b8ea7d6dae51fef7fcbb69da05874a7d80a040e
Thomas Schank <DrTom@schank.ch>
2012-10-26 13:09:52 +0200

    added routes and index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ HackitzOtterlog::Application.routes.draw do
   match "otterlogs/" => "otterlogs#index" , via: 'get'
   match "otterlogs/:id" => "otterlogs#get" , via: 'get'
   match "otterlogs/:id" => "otterlogs#put" , via: 'put'
+
+  match '*path' => "main#index"
+
+  root to: 'main#index'
   
 end
